e8bbdfd40e2331d678224970fa59abeaa3f6c00f
Peter Andersson <pjotor87@hotmail.com>
2017-05-07 09:35:30 +0200

    Initial commit

---
+++ RunBatFileWithSameNameAsThisExeFileInSameFolder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+
+namespace RunBatFileWithSameNameAsThisExeFileInSameFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.Start(Path.ChangeExtension(Assembly.GetEntryAssembly().Location, "bat"));
+        }
+    }
+}
